791ef9bd12f7149fed194dded4481ec597f0debe
bigmac7 <maxcharuta@gmail.com>
2025-09-18 09:37:14 +0100

    clean-up main and fix func name

--- src/getter.rs
+++ src/getter.rs
@@ -20,3 +20,26 @@ pub fn get_verse(book: &str, chapter: i32, verse: i32) -> Result<Vec<String>, Bo
     }
     Ok(data)
 }
+
+pub fn get_available_translations() -> Result<Vec<String>, Box<dyn Error>> {
+    let con = conn("KJV.db")?;
+    let mut stmt = con.prepare("SELECT * FROM translations")?;
+    let rows = stmt.query_map([], |row| row.get(0))?;
+    let mut data = Vec::new();
+    for row in rows {
+        data.push(row?);
+    }
+    Ok(data)
+}
+
+pub fn get_chapter(book: &str, chapter: i32) -> Result<Vec<String>, Box<dyn Error>> {
+    let con = conn("KJV.db")?;
+    let book_id = get_book_id(book)?;
+    let mut stmt = con.prepare("SELECT text FROM kjv_verses WHERE book_id = ? AND chapter = ?")?;
+    let rows = stmt.query_map([book_id, chapter], |row| row.get(0))?;
+    let mut data = Vec::new();
+    for row in rows {
+        data.push(row?);
+    }
+    Ok(data)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ mod db;
 mod getter;
 mod structs;
 
-use getter::get_verse;
+use getter::{get_available_translations, get_verse};
+use std::error::Error;
 
-fn main() {
-    match get_verse("Genesis", 1, 1) {
-        Ok(verses) => {
-            for verse in verses {
-                println!("{}", verse);
-            }
-        }
-        Err(e) => {
-            eprintln!("Error: {}", e);
-        }
+fn main() -> Result<(), Box<dyn Error>> {
+    let verses = get_verse("Genesis", 1, 1)?;
+    for verse in verses {
+        println!("{}", verse);
     }
+
+    let translations = get_available_translations()?;
+    for translation in translations {
+        println!("{}", translation);
+    }
+    Ok(())
 }
