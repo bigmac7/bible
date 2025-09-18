5e43cee5beaf960bfa262cbdf6bcd3e20de1cdae
bigmac7 <maxcharuta@gmail.com>
2025-09-18 10:53:30 +0100

    add clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 mod db;
 mod getter;
 
+use clap::Parser;
 use getter::{get_available_translations, get_verse};
 use std::error::Error;
 
+#[derive(Parser)]
+struct Cli {
+    book: String,
+    chapter: i32,
+    verse: i32,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let verses = get_verse("Genesis", 1, 1)?;
+    let args = Cli::parse();
+    let verses = get_verse(&args.book, args.chapter, args.verse)?;
     for verse in verses {
         println!("{}", verse);
-    }
-
-    let translations = get_available_translations()?;
-    for translation in translations {
-        println!("{}", translation);
     }
     Ok(())
 }
